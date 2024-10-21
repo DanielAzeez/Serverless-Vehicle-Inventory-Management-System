9a77197e93830c3146ff006b6e555aad4a2e94b8
Daniel Azeez <98490919+DanielAzeez@users.noreply.github.com>
2024-10-21 21:48:16 +0100

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,73 +1,81 @@
+# AWS SDK for Python (Boto3) - https://aws.amazon.com/sdk-for-python/
 import boto3
 from botocore.exceptions import ClientError
 from boto3.dynamodb.conditions import Key
 import logging
 import json
 
-# Set up logging
+# AWS Lambda Function Logging in Python - https://docs.aws.amazon.com/lambda/latest/dg/python-logging.html
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-# Initialize DynamoDB resource
 session = boto3.Session()
 dynamodb = session.resource("dynamodb")
 
-# Replace with the DynamoDB table name
-table_name = "locations"
+table_name = "locations"  # Change this to the actual table name in your environment
 table = dynamodb.Table(table_name)
 
-# Create an item in DynamoDB
+# DynamoDB create item in table logic
 def create_item(item):
     try:
-        response = table.put_item(Item=item)
-        logger.info({"operation": "create_item", "details": response})
+        ret = table.put_item(Item=item)
+        logger.info({"operation": "create an item", "details": ret})
     except ClientError as err:
-        logger.error({"operation": "create_item_error", "details": str(err)})
+        print(err)
+        logger.debug({"operation": "item creation", "details": err})
 
-# Update an item in DynamoDB
-def update_item(item_id, reserve):
+# DynamoDB update Item from table logic
+def update_item(item, reserve):
     try:
-        response = table.update_item(
-            Key={'id': item_id},
-            UpdateExpression="set reserve = :r",
-            ExpressionAttributeValues={':r': reserve},
+        ret = table.update_item(
+            Key={'id': item["id"]},
+            UpdateExpression="set reserve = :ad",
+            ExpressionAttributeValues={':ad': reserve},
             ReturnValues="UPDATED_NEW"
         )
-        logger.info({"operation": "update_item", "details": response})
+        logger.info({"operation": "update an item", "details": ret})
     except ClientError as err:
-        logger.error({"operation": "update_item_error", "details": str(err)})
+        logger.debug({"operation": "item update", "details": err})
 
-# Retrieve an item from DynamoDB
-def get_item(item_id):
+# DynamoDB get Item from table logic
+def get_item(id):
     try:
-        response = table.get_item(Key={'id': item_id})
-        if 'Item' in response:
-            return response['Item']
-        else:
-            logger.info(f"Item with ID {item_id} not found")
-            return None
+        ret = table.get_item(Key={'id': id})
+        logger.info({"operation": "query an item", "details": ret})
+        return ret['Item']
     except ClientError as err:
-        logger.error({"operation": "get_item_error", "details": str(err)})
+        logger.debug({"operation": "item query", "details": err})
 
-# Lambda function handler
+# function to handle API Gateway Lambda proxy integration event
+def api_gateway_handler(event):
+    body = json.loads(event['body'])
+    item = body['vehicle']
+    item_id = item['id']
+    create_item(item)
+    update_item(item, "yes")
+    ret = get_item(item_id)
+    
+    return {
+        'statusCode': '200',
+        'body': json.dumps(ret),
+        'headers': {
+            'Content-Type': 'application/json',
+        }
+    }
+
+# Function to handle Lambda Local Test
+def local_test_handler(event):
+    item = event['vehicle']
+    item_id = item['id']
+    create_item(item)
+    update_item(item, "yes")
+    ret = get_item(item_id)
+    return ret
+
 def lambda_handler(event, context):
+    logger.info(event)
     try:
-        body = json.loads(event['body'])
-        item = body['vehicle']
-        item_id = item['id']
-
-        create_item(item)
-        update_item(item_id, "yes")
-        vehicle = get_item(item_id)
-
-        return {
-            'statusCode': 200,
-            'body': json.dumps(vehicle),
-            'headers': {'Content-Type': 'application/json'}
-        }
-    except Exception as e:
-        logger.error(f"Error processing request: {str(e)}")
-        return {
-            'statusCode': 500,
-            'body': json.dumps({"error": "Internal Server Error"})
-        }
+        ret = api_gateway_handler(event)
+    except:
+        ret = local_test_handler(event)
+    return ret
